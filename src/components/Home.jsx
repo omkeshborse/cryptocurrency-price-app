114a1420c7e4aa348e25ff2bfb0816410aa31714
omkeshborse <bomkes25@gmail.com>
2023-08-29 13:53:54 +0530

    feat : home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,13 @@
+import {Box, Image, Text} from '@chakra-ui/react'
+import btc from '../assets/btc.png'
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box bgColor={'blackAlpha.900'} w={'full'}  h={"80vh"}>
+    <Image src={btc} w={'full'} h={'full'} objectFit={'contain'} filter={'grayScale(1)'}/>
+    <Text  fontSize={'6xl'} textAlign={'center'} fontWeight={'thin'} color={'whiteAlpha.700'} mt={"-20"}>
+      GeekCrypto
+    </Text>
+    </Box>
   )
 }
 export default Home
